77ac7ad00448adf4e5c28c0d963332c4daf094f2
Rob Martin <rob.martin.5758@gmail.com>
2020-06-05 16:10:34 -0400

    initial

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>rob@robmartin.dev</p>
+            <p>222-333-4444</p>
+            <p>@remrobem</p>
+        </div>
+    )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+// export default function Home() {
+//   return <div>Bootcamp</div>
+// }
+
+// export default () => <div>Hey, Bootcamp</div>
+
+const IndexPage = () => {
+
+  return (
+    <div>
+      <h1>Hey</h1>
+      <h2>
+        I'm Rob.
+      </h2>
+    </div>
+  )
 }
+
+export default IndexPage
