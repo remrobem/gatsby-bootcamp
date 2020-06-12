1e82f46ee12b6a5ff47f7e83562a80eb7354f8ab
Rob Martin <rob.martin.5758@gmail.com>
2020-06-12 00:01:48 -0400

    added layout and some styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Header from '../components/header'
-import Footer from '../components/footer'
+// import Header from '../components/header'
+// import Footer from '../components/footer'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
+
     return (
-        <div>
-            <Header />
-            <h1>About</h1>
+        <Layout>
+<h1>About</h1>
             <h2>Rob</h2>
             <h3>Bio</h3>
             <p>Rob is smart</p>
             <p>Rob works hard</p>
             <p>Rob wants to help</p>
             <p>So, <Link to='/contact'>Contact Rob</Link></p>
-            <Footer />
-        </div>
+        </Layout>
     )
+    // return (
+    //     <div>
+    //         <Header />
+    //         <h1>About</h1>
+    //         <h2>Rob</h2>
+    //         <h3>Bio</h3>
+    //         <p>Rob is smart</p>
+    //         <p>Rob works hard</p>
+    //         <p>Rob wants to help</p>
+    //         <p>So, <Link to='/contact'>Contact Rob</Link></p>
+    //         <Footer />
+    //     </div>
+    // )
 }
 
 export default AboutPage
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import Header from '../components/header'
-import Footer from '../components/footer'
+// import Header from '../components/header'
+// import Footer from '../components/footer'
+import Layout from '../components/layout'
 
 const ContactPage = () => {
+
     return (
-        <div>
-            <Header />
+        <Layout>
             <h1>Contact</h1>
             <p>rob@robmartin.dev</p>
             <p>222-333-4444</p>
-            <a href='https://twitter.com/remrobem' target='_blank'>@remrobem</a>
-            <Footer />
-        </div>
-
+            <a href='https://twitter.com/remrobem' target='_blank' rel="noreferrer">@remrobem</a>
+        </Layout>
     )
+    // return (
+    //     <div>
+    //         <Header />
+    //         <h1>Contact</h1>
+    //         <p>rob@robmartin.dev</p>
+    //         <p>222-333-4444</p>
+    //         <a href='https://twitter.com/remrobem' target='_blank' rel="noreferrer">@remrobem</a>
+    //         <Footer />
+    //     </div>
+
+    // )
 
 }
 
