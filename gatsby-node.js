a37c25e3f71ac5dc5b081395d4f6b91566ab1985
Rob Martin <rob.martin.5758@gmail.com>
2020-06-16 19:34:36 -0400

    completed contenful implementation

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,21 +2,22 @@
 
 const path = require("path")
 // used to create slug for markdown file.slug used in url for page displaying markdown
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
+// not needed when using contentful which has slug already defined
+// module.exports.onCreateNode = ({ node, actions }) => {
+//   const { createNodeField } = actions
 
-  if (node.internal.type === "MarkdownRemark") {
-    console.log("node: ", JSON.stringify(node, undefined, 4))
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-    console.log("slug: ", slug)
+//   if (node.internal.type === "MarkdownRemark") {
+//     console.log("node: ", JSON.stringify(node, undefined, 4))
+//     const slug = path.basename(node.fileAbsolutePath, ".md")
+//     console.log("slug: ", slug)
 
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    })
-  }
-}
+//     createNodeField({
+//       node,
+//       name: "slug",
+//       value: slug,
+//     })
+//   }
+// }
 
 module.exports.createPages = async ({ graphql, actions }) => {
   // console.log('graphql: ', graphql)
@@ -26,28 +27,51 @@ module.exports.createPages = async ({ graphql, actions }) => {
   const blogTemplate = path.resolve("./src/templates/blog.js")
   //   2. get markdown date
   // graphql returns promise
-  const res = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-          }
-        }
+  // const res = await graphql(`
+  //   query {
+  //     allMarkdownRemark {
+  //       edges {
+  //         node {
+  //           fields {
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+const res = await graphql(`
+query {
+  allContentfulBlogPost {
+    edges {
+      node {
+        slug
       }
     }
-  `)
+  }
+}
+`)
+
   //   3. create pages
   // console.log('response: ', JSON.stringify(res, undefined, 4))
   // console.log('response error: ', res.error)
-  res.data.allMarkdownRemark.edges.forEach(edge => {
+  // res.data.allMarkdownRemark.edges.forEach(edge => {
+  //   createPage({
+  //     component: blogTemplate,
+  //     path: `/blog/${edge.node.fields.slug}`,
+  //     context: {
+  //       slug: edge.node.fields.slug,
+  //     },
+  //   })
+  // })
+
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
-        slug: edge.node.fields.slug,
+        slug: edge.node.slug,
       },
     })
   })
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,53 +1,87 @@
 import React from "react"
 import Layout from "../components/layout"
 import { Link, graphql, useStaticQuery } from "gatsby"
-import styles from './blog.module.scss'
+import styles from "./blog.module.scss"
 
 const BlogPage = () => {
   const blogHeader = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            publishedDate(formatString: "MMMM D, YYYY")
+            slug
           }
         }
       }
     }
   `)
-  console.log("blogHeader: ", blogHeader)
-  return (
-    <Layout>
-      <div>
-        <ol className={styles.posts}>
-          {blogHeader.allMarkdownRemark.edges.map(edge => {
-            return (
-              <li className={styles.post} key={edge.node.frontmatter.title}>
-                <Link to={`/blog/${edge.node.fields.slug}`}>
-                  <h2>{edge.node.frontmatter.title}</h2>
-                </Link>
-                <p>{edge.node.frontmatter.date}</p>
-              </li>
-            )
-          })}
-        </ol>
-      </div>
-    </Layout>
-  )
-  // return (
-  //     <div>
-  //         <Header />
-  //         <h1>BLOGS</h1>
-  //         <p>posts will show up here</p>
-  //         <Footer />
-  //     </div>
-  // )
+
+return (
+  <Layout>
+    <div>
+      <ol className={styles.posts}>
+        {blogHeader.allContentfulBlogPost.edges.map(edge => {
+          return (
+            <li className={styles.post} key={edge.node.title}>
+              <Link to={`/blog/${edge.node.slug}`}>
+                <h2>{edge.node.title}</h2>
+              </Link>
+              <p>{edge.node.publishedDate}</p>
+            </li>
+          )
+        })}
+      </ol>
+    </div>
+  </Layout>
+)
 }
+  // const BlogPage = () => {
+  //   const blogHeader = useStaticQuery(graphql`
+  //     query {
+  //       allMarkdownRemark {
+  //         edges {
+  //           node {
+  //             frontmatter {
+  //               title
+  //               date
+  //             }
+  //             fields {
+  //               slug
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `)
+  // console.log("blogHeader: ", blogHeader)
+  // return (
+  //   <Layout>
+  //     <div>
+  //       <ol className={styles.posts}>
+  //         {blogHeader.allMarkdownRemark.edges.map(edge => {
+  //           return (
+  //             <li className={styles.post} key={edge.node.frontmatter.title}>
+  //               <Link to={`/blog/${edge.node.fields.slug}`}>
+  //                 <h2>{edge.node.frontmatter.title}</h2>
+  //               </Link>
+  //               <p>{edge.node.frontmatter.date}</p>
+  //             </li>
+  //           )
+  //         })}
+  //       </ol>
+  //     </div>
+
+  
+// return (
+//     <div>
+//         <Header />
+//         <h1>BLOGS</h1>
+//         <p>posts will show up here</p>
+//         <Footer />
+//     </div>
+// )
+// }
 
 export default BlogPage
